d22469ae3005154572e7a8d808ae2e8074bc848f
aarondevon <me@aarondevon.com>
2017-04-07 08:46:45 -0500

    add methods for validating name, and numbers, add gaurd clause for name and loan amount

--- 2_small_programs/mortgage_car_loan_calculator.rb
+++ 2_small_programs/mortgage_car_loan_calculator.rb
@@ -1,6 +1,61 @@
+require 'yaml'
+
+MESSAGES = YAML.load_file('mortgage_car_loan_messages.yml')
+
+# Languages supported are
+# English: eng
+# German: ger
+LANGUAGE = 'eng'
+
+# Get users name
 # Get loan amount
 # Get APR
 # Get loan duration in years
 
 # Calculate monthly interest
 # Calculate loan duration in months
+
+# reference Kernel module and use parentheses () for methods
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def messages(message, lang='eng')
+  MESSAGES[lang][message]
+end
+
+def valid_number?(input)
+  integer?(input) || float?(input)
+end
+
+def integer?(input)
+  input == input.to_i().to_s()
+end
+
+def float?(input)
+  input == input.to_f().to_s()
+end
+
+prompt(messages('welcome', LANGUAGE))
+
+name = ''
+loop do
+  name = gets().chomp()
+  break if !name.empty?()
+  prompt(messages('valid_name', LANGUAGE))
+end
+
+prompt("#{messages('hi_name', LANGUAGE)} #{name}")
+
+loop do # main loop
+  prompt(messages('loan_amount', LANGUAGE))
+
+  loan_amount= ''
+  loop do
+    loan_amount = gets.chomp
+    break if valid_number?(loan_amount)
+    prompt(messages('number_error', LANGUAGE))
+  end
+
+end
