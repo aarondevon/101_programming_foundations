bb163883e79209e1c6fed64a6381ca83af837791
aarondevon <me@aarondevon.com>
2017-04-19 08:24:34 -0500

    create second sets of win conditions

--- 2_small_programs/bonus_rock_paper_scissors_lizard_spock.rb
+++ 2_small_programs/bonus_rock_paper_scissors_lizard_spock.rb
@@ -7,5 +7,15 @@ end
 def win_conditions_one?(first, second)
   first == 'rock' && second == 'scissors' ||
     first == 'paper' && second == 'rock' ||
-    first == 'scissors' && second == 'paper'
+    first == 'scissors' && second == 'paper' ||
+    first == 'rock' && second == 'lizard' ||
+  first == 'lizard' && second == 'spock'
+end
+
+def win_conditions_two?(first, second)
+  first == 'spock' && second == 'scissors' ||
+  first == 'scissors' && second == 'lizard' ||
+  first == 'lizard' && second == 'paper' ||
+  first == 'paper' && second == 'spock' ||
+  first == 'spock' && second == 'rock'
 end
