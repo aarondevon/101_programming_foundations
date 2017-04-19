879b1dc4205eee622cf9733d3f20ea922ae334bb
aarondevon <me@aarondevon.com>
2017-04-19 08:34:45 -0500

    create main loop and add game logic

--- 2_small_programs/bonus_rock_paper_scissors_lizard_spock.rb
+++ 2_small_programs/bonus_rock_paper_scissors_lizard_spock.rb
@@ -29,3 +29,27 @@ def display_results(player, computer)
     prompt("It's a tie!")
   end
 end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = ["rock", "paper", "scissors", "lizard", "spock"].sample()
+
+  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase.start_with?('y')
+end
