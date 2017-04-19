840d5044e522d7d627adbdd1c225d062a51d840b
aarondevon <me@aarondevon.com>
2017-04-19 08:16:18 -0500

    create first sets of win conditions

--- 2_small_programs/bonus_rock_paper_scissors_lizard_spock.rb
+++ 2_small_programs/bonus_rock_paper_scissors_lizard_spock.rb
@@ -3,3 +3,9 @@ VALID_CHOICES = %w(rock paper scissors lizard spock)
 def prompt(message)
   Kernel.puts("message")
 end
+
+def win_conditions_one?(first, second)
+  first == 'rock' && second == 'scissors' ||
+    first == 'paper' && second == 'rock' ||
+    first == 'scissors' && second == 'paper'
+end
