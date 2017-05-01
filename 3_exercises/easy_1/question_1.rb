160d878d3d9fb2197b2b1ec8a245b8c1e01ec9f5
aarondevon <me@aarondevon.com>
2017-05-01 08:07:21 -0500

    complete question one

---
+++ 3_exercises/easy_1/question_1.rb
@@ -0,0 +1,24 @@
+What would you expect the code below to print out?
+
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+=begin
+uniq creates a new array removing and duplicate objects.
+Therefore, puts will print the original array
+1
+2
+2
+3
+
+puts calls to_s on the element
+if we wanted a different output we could have used p which
+calls .inspect on it's argument.
+
+also we could have written:
+puts numbers.inspect
+and gotten the same results as
+p numbers
+=end
