bf20099724745b4156bd7ef75af58ea517d873bd
aarondevon <me@aarondevon.com>
2017-05-11 07:47:04 -0500

    How could the unnecessary duplication in this method be removed

---
+++ 3_exercises/medium_2/5_question.rb
@@ -0,0 +1,15 @@
+# How could the unnecessary duplication in this method be removed?
+
+=begin
+def color_valid(color)
+  if color == "blue" || color == "green"
+    true
+  else
+    false
+  end
+end
+=end
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
