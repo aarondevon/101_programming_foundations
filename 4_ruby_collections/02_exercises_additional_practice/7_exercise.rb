594f25fd11fa5d9c530b9b75619c009defdf2d8f
aarondevon <me@aarondevon.com>
2017-05-23 08:09:52 -0500

    sort results

--- 4_ruby_collections/02_exercises_additional_practice/7_exercise.rb
+++ 4_ruby_collections/02_exercises_additional_practice/7_exercise.rb
@@ -4,3 +4,15 @@ statement = "The Flintstones Rock"
 # ex:
 
 # { "F"=>1, "R"=>1, "T"=>1, "c"=>1, "e"=>2, ... }
+
+letter_count = {}
+
+statement.split('').sort.each do |letter|
+  if letter == ' '
+    next
+  else
+  letter_count[letter] = statement.count(letter)
+  end
+end
+
+p letter_count
