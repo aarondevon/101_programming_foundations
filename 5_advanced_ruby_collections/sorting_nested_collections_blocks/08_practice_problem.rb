77967751347aafc51dd68385565f28abb784e4d3
aarondevon <me@aarondevon.com>
2017-06-20 10:35:28 -0500

    initial commit

---
+++ 5_advanced_ruby_collections/sorting_nested_collections_blocks/08_practice_problem.rb
@@ -0,0 +1,4 @@
+# Using the each method, write some code to output all of the vowels from the
+# strings.
+
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
