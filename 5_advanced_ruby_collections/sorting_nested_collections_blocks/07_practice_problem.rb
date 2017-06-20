d9c89a6b401bd161459462bb97a095fb0ead4e25
aarondevon <me@aarondevon.com>
2017-06-20 10:33:50 -0500

    what are the final values of a and b

--- 5_advanced_ruby_collections/sorting_nested_collections_blocks/07_practice_problem.rb
+++ 5_advanced_ruby_collections/sorting_nested_collections_blocks/07_practice_problem.rb
@@ -1,6 +1,5 @@
-Practice Problem 7
-
-Given this code, what would be the final values of a and b? Try to work this out without running the code.
+# Given this code, what would be the final values of a and b? Try to work this
+# out without running the code.
 
 a = 2
 b = [5, 8]
@@ -8,3 +7,7 @@ arr = [a, b]
 
 arr[0] += 2
 arr[1][0] -= a
+
+# a is 2
+# b is [3, 8]
+# variable 'a' was never modified, the value in the array was modified
