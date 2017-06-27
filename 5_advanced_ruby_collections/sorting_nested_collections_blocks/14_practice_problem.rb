829cf0bf57faf2a84fa61ef8a90b8a153ab0603a
aarondevon <me@aarondevon.com>
2017-06-27 10:09:06 -0500

    capitalize colors of fruits, upcase vegetable size, return an array containing the changes

---
+++ 5_advanced_ruby_collections/sorting_nested_collections_blocks/14_practice_problem.rb
@@ -0,0 +1,21 @@
+# Given this data structure write some code to return an array containing the
+# colors of the fruits and the sizes of the vegetables. The sizes should be
+# uppercase and the colors should be capitalized.
+
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+
+hsh.map do |_, hsh|
+  if hsh[:type] == 'fruit'
+    hsh[:colors].map do |color|
+      color.capitalize
+    end
+  elsif hsh[:type] == 'vegetable'
+    hsh[:size].upcase
+  end
+end
