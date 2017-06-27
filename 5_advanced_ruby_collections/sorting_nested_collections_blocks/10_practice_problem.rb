698429bda673a4b41a7cb1c5d23c8242c6a7de0e
aarondevon <me@aarondevon.com>
2017-06-27 07:47:07 -0500

    return identical structure but increment value by 1

--- 5_advanced_ruby_collections/sorting_nested_collections_blocks/10_practice_problem.rb
+++ 5_advanced_ruby_collections/sorting_nested_collections_blocks/10_practice_problem.rb
@@ -2,4 +2,9 @@
 # use the map method to return a new array identical in structure to the
 # original but where the value of each integer is incremented by 1.
 
-[{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+[{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |hash|
+  incremented = hash.map do |letter, number|
+    [letter, number += 1]
+  end
+  incremented.to_h
+end
