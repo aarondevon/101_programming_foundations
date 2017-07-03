cfca04cf3099616816c3537a15c826b414afc980
aarondevon <me@aarondevon.com>
2017-07-03 09:19:07 -0500

    update board to variable, create player move

--- 6_slightly_larger_programs/tictactoe.rb
+++ 6_slightly_larger_programs/tictactoe.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def prompt(str)
   puts "==> #{str}"
 end
@@ -6,7 +8,7 @@ def display_game_start
   puts "Let's play Tic Tac Toe!"
 end
 
-def display_board
+def display_board(board)
   puts ""
   puts "     |     |     "
   puts "  #{board[0]}  |  #{board[1]}  |  #{board[2]}  "
@@ -22,17 +24,33 @@ def display_board
   puts ""
 end
 
-def board()
-  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-end
+# def board(board)
+#   [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# end
 
 def prompt_player_move
-  prompt("Please make your move")
+  prompt("Enter a number 1-9 to place your X")
 end
+
+def player_position
+  gets.chomp.to_i
+end
+
+def player_move(board)
+  board[player_position] = "X"
+end
+
 
 
 display_game_start
 
 loop do
-  display_board
+  display_board(board)
+
+  prompt_player_move
+  player_move(board)
+
+
+  display_board(board)
+  break
 end
