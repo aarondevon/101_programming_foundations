854a5d48b11701ee87f02e48f82e11c3eeca3041
aarondevon <me@aarondevon.com>
2017-07-03 08:48:53 -0500

    create board method

--- 6_slightly_larger_programs/tictactoe.rb
+++ 6_slightly_larger_programs/tictactoe.rb
@@ -1,17 +1,20 @@
 def display_board
   puts ""
   puts "     |     |     "
-  puts "     |     |     "
-  puts "     |     |     "
-  puts "-----+-----+-----"
-  puts "     |     |     "
-  puts "     |     |     "
+  puts "  #{board[0]}  |  #{board[1]}  |  #{board[2]}  "
   puts "     |     |     "
   puts "-----+-----+-----"
   puts "     |     |     "
+  puts "  #{board[3]}  |  #{board[4]}  |  #{board[5]}  "
   puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{board[6]}  |  #{board[7]}  |  #{board[7]}  "
   puts "     |     |     "
   puts ""
 end
 
-display_board
+def board
+  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+end
+  display_board
