1f583eb53710942527053f38c7e6bb8d5e29b646
aarondevon <me@aarondevon.com>
2017-07-11 09:17:24 -0500

    add board full method

--- 6_slightly_larger_programs/02_tictactoe.rb
+++ 6_slightly_larger_programs/02_tictactoe.rb
@@ -44,10 +44,22 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def computer_places_piece!(brd)
+  square = empty_squares.brd.sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
 board = initialize_board
-
 display_board(board)
 
-player_places_piece!(board)
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
 
-display_board(board)
+  break if someone_won? || board_full?
+end
